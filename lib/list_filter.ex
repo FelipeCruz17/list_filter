0491cf191f974a3b163ba3f84513c0fe29116d48
FelipeCruz17 <feemattioli@gmail.com>
2023-01-30 20:12:34 -0300

    elixir-introduction-challenge02

---
+++ lib/list_filter.ex
@@ -0,0 +1,20 @@
+defmodule ListFilter do
+  import Integer, only: [is_odd: 1]
+
+  def call(list), do: list |> get_numbers_from_list |> get_odd_numbers_from_list |> length
+
+  defp get_numbers_from_list(string_list) do
+    string_list
+    |> Enum.map(fn element ->
+      case Integer.parse(element) do
+        {result, _remainder_of_binary} -> result
+        status -> status
+      end
+    end)
+    |> Enum.filter(fn element -> is_integer(element) end)
+  end
+
+  defp get_odd_numbers_from_list(integers_list) do
+    integers_list |> Enum.filter(fn element -> is_odd(element) end)
+  end
+end
